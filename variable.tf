7d667655b3f2156fefb5466c72407429a2f46915
prakash6333 <prakashy6333@gmail.com>
2025-09-10 15:14:17 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,9 @@
 variable "filename" {
-  
+  description = "The name of the file"
+  type        = string
 }
+
 variable "content" {
- 
+  description = "The content of the file"
+  type        = string
 }
