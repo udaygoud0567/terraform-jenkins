8bf9e516c2ea0eaa2ba768eb34b7d126083d664d
prakash6333 <prakashy6333@gmail.com>
2025-09-11 15:27:13 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,7 @@
 variable "filename" {
-  description = "The name of the file"
-  type        = string
-}
-
-variable "content" {
-  description = "The content of the file"
-  type        = string
+default = [
+"pets.txt",
+"cats.txt",
+"dogs.txt"
+]
 }
